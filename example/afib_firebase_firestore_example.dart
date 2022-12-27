4836e536f7b1b9543528983bb9dcaf2e430940f8
Chris Jones <chris@xclarus.com>
2022-12-27 08:41:52 -0600

    first commit

---
+++ example/afib_firebase_firestore_example.dart
@@ -0,0 +1,6 @@
+import 'package:afib_firebase_firestore/afib_firebase_firestore.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/afib_firebase_firestore.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library afib_firebase_firestore;
+
+export 'src/afib_firebase_firestore_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
